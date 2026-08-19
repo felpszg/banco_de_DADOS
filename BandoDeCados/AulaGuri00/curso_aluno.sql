44ace987fa5b0115aa2ff0593bcb357635868675
felpszg <felipegabriel@unipam.edu.br>
2026-08-19 23:08:05 +0000

    19/08

--- BandoDeCados/AulaGuri00/curso_aluno.sql
+++ BandoDeCados/AulaGuri00/curso_aluno.sql
@@ -1,4 +1,4 @@
--- Active: 1786666758290@@127.0.0.1@5432@bd_aula@public
+-- Active: 1787176816834@@127.0.0.1@5432@bd_aula@public
 CREATE TABLE curso(
     id_curso INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nome VARCHAR(60) NOT NULL
@@ -8,3 +8,52 @@ CREATE TABLE aluno(
     nome VARCHAR(80) NOT NULL,
     id_curso INTEGER NOT NULL REFERENCES curso(id_curso)
 );
+
+SELECT * FROM  curso;
+
+SELECT id_curso FROM  curso;
+SELECT nome FROM  curso;
+
+SELECT * FROM  aluno;
+
+SELECT id_aluno FROM  aluno;
+SELECT id_curso FROM  aluno;
+SELECT nome FROM  aluno;
+
+INSERT INTO curso (nome) VALUES
+('Sistemas de Informacao'),
+('Administracao'),
+('Direito'),
+('Ciencia da computacao');
+
+INSERT INTO aluno (nome,id_curso) VALUES
+('Ana Beatriz Sousa' , 1),
+('Carlos Henrique Lemos' , 1),
+('Pedro Mestre Mago' , 2),
+('Davi Senior Silva' , 3),
+('Gustavo Pereira Manuel' , 1);
+
+SELECT 
+    a.id_aluno AS id,
+    a.nome AS alunos,
+    a.id_curso
+FROM
+    aluno a 
+ORDER BY
+    nome ASC;
+
+SELECT
+    id_curso AS id,
+    nome AS cursos
+FROM
+    curso
+ORDER BY
+    nome ASC;
+
+SELECT
+    nome AS aluno,
+     id_curso AS id
+FROM
+    aluno
+WHERE
+    id_curso = 1;
